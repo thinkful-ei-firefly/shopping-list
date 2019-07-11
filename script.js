0029498e47ea3f491800191ac612197739fcd9fc
Sam Walker <jack.nasal@gmail.com>
2019-07-11 15:45:15 -0400

    finished shopping list

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
+'use strict';
+
 function addItem() {
-    $('#js-shopping-list-form').submit(event => {
-        event.preventDefault();
-        const userInput = $('#shopping-list-entry').val();
-        $('ul').append(`<li>
-            <span class="shopping-item">${userInput}</span>
+  $('#js-shopping-list-form').submit(event => {
+    event.preventDefault();
+    let userInput = $('#shopping-list-entry');
+    if (userInput.val() !== '') {
+      $('ul').append(`<li>
+            <span class="shopping-item">${userInput.val()}</span>
             <div class="shopping-item-controls">
               <button class="shopping-item-toggle">
                 <span class="button-label check-label">check</span>
               </button>
               <button class="shopping-item-delete">
-                <span class="button-label">delete</span>
+                <span class="button-label delete-label">delete</span>
               </button>
             </div>
-          </li>`)
-    })
+          </li>`);
+      userInput.val('');
+    }
+  });
 }
 
 function checkUncheckItem() {
-    $('li').on('click', $('.check-label'), function() {
-        const checkedText = 
-        checkText.toggleClass('.shopping-item__checked'); 
-    }); 
+  $('ul').on('click', '.check-label', function() {
+    const checkedText = $(event.target).closest('li').find('.shopping-item');
+    checkedText.toggleClass('shopping-item__checked');
+  }); 
 }
 
 function removeItem() {
-
+  $('ul').on('click', '.delete-label', function() {
+    const itemToDel = $(event.target).closest('li');
+    itemToDel.remove();
+  }); 
 }
 
 function shoppingList() {
-    addItem();
-    checkUncheckItem();
-    removeItem();
+  addItem();
+  checkUncheckItem();
+  removeItem();
 }
 
 $(shoppingList);
